089995ca9c279656a66487b449d78400fbf6b0d5
Guillermo <guillermo.marin@roraimadevs.com>
2022-12-11 15:32:44 -0400

    updated the code and added :date? route

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const dateFun = require('./services/date');
+require('dotenv').config({path: __dirname + '/.env'})
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -25,8 +27,16 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+app.get('/api/:date?', (req, res) => {
+  const body = req.params;
+  console.log(body)
+  
+  const date = dateFun.getDate(body.date);
+
+  res.json(date);
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
